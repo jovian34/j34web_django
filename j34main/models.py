0159efcb5a03a7309b41db90c0c3cac3dc099f49
Carl James <carl@jovian34.com>
2022-07-16 15:16:20 -0400

    made cats m2m

--- j34main/models.py
+++ j34main/models.py
@@ -29,7 +29,7 @@ class Content(models.Model):
     image_caption = models.CharField(default='', blank=True, max_length=100)
     teaser = models.TextField(null=False)
     content = models.TextField(null=False)
-    # category = models.ManyToManyField(Category, default=2)
+    categories = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.title
