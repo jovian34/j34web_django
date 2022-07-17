acdece2d04bc643ef0f2b4106a1385fcc5f2dbfa
Carl James <carl@jovian34.com>
2022-07-16 23:48:36 -0400

    added categories to tests - all pass

--- j34main/tests.py
+++ j34main/tests.py
@@ -3,12 +3,20 @@ import datetime
 from django.test import Client, TestCase
 from django.utils import timezone
 
-from .models import Content
+from .models import Content, Category
 
 
 class ContentViewTests(TestCase):
     def setUp(self):
         self.now = timezone.now()
+        self.cat1 = Category.objects.create(cat_name="All")
+        self.cat1.save()
+        self.cat2 = Category.objects.create(cat_name="Tech")
+        self.cat2.save()
+        self.cat3 = Category.objects.create(cat_name="Coding")
+        self.cat3.save()
+        self.cat4 = Category.objects.create(cat_name="Video")
+        self.cat4.save()
         self.c1 = Content.objects.create(
             title="Carl's First Blog",
             sub_title="first",
@@ -34,8 +42,12 @@ class ContentViewTests(TestCase):
                     "takes more than a solid Wi-Fi connection and "
                     "decent web conferencing software; it takes careful "
                     "planning, effective facilitation and the right "
-                    "mix of channels</p>", )
+                    "mix of channels</p>",
+            )
         self.c1.save()
+        self.c1.categories.set([self.cat1, self.cat2])
+
+
         self.c2 = Content.objects.create(
             title="Carl's Second Blog",
             pub_date=self.now - datetime.timedelta(days=27),
@@ -70,8 +82,11 @@ class ContentViewTests(TestCase):
                     "the new agreement, companies must foot "
                     "the bill for the measures, test workers "
                     "every two weeks and share those plans "
-                    "with New Mexico health officials.</p>", )
+                    "with New Mexico health officials.</p>",
+            )
         self.c2.save()
+        self.c2.categories.set([self.cat1, self.cat3])
+
         self.c3 = Content.objects.create(
             title="Carl's Third Blog",
             sub_title="third",
@@ -101,8 +116,11 @@ class ContentViewTests(TestCase):
                     "provided by N95s, which stop at least "
                     "95% of very small particles with a "
                     "sophisticated filter and a snug fit to "
-                    "the face. ", )
+                    "the face. ",
+            )
         self.c3.save()
+        self.c3.categories.set([self.cat1, self.cat3])
+
         self.c4 = Content.objects.create(
             title="Carl's Fourth Blog",
             sub_title="fourth",
@@ -130,8 +148,11 @@ class ContentViewTests(TestCase):
                     "Valley tech giants such as Facebook Inc., Twitter Inc. "
                     "and Alphabet Inc.’s Google and YouTube by giving them "
                     "broad immunity for the content they publish from users "
-                    "on their sites.<.p>", )
+                    "on their sites.<.p>",
+            )
         self.c4.save()
+        self.c4.categories.set([self.cat1, self.cat3, self.cat4])
+
         self.c5 = Content.objects.create(
             title="Carl's Fifth Blog",
             sub_title="fifth",
@@ -146,8 +167,11 @@ class ContentViewTests(TestCase):
             content="<p>Type in the letters you want to "
                     "unscramble and our word finder will "
                     "show you all the possible words you "
-                    "can make from the letters in your hand.</p>", )
+                    "can make from the letters in your hand.</p>",
+            )
         self.c5.save()
+        self.c5.categories.set([self.cat1, self.cat2])
+
         self.c6 = Content.objects.create(
             title="Carl's Sixth Blog",
             sub_title="third",
@@ -164,8 +188,10 @@ class ContentViewTests(TestCase):
                     "kingdom was at an end, though "
                     "during the last years of Eadwig's "
                     "reign the Mercians and Northumbrians "
-                    "set up Eadgar as king.</p>", )
+                    "set up Eadgar as king.</p>",
+            )
         self.c6.save()
+        self.c6.categories.set([self.cat1, self.cat4])
 
     def test_index_page_forwards(self):
         client = Client()
@@ -175,13 +201,14 @@ class ContentViewTests(TestCase):
     def test_sub_index_page_renders(self):
         client = Client()
         response = client.get('/j34/')
-        contain_text = "its existence as a separate"
+        contain_text = "Video"
         self.assertContains(response, contain_text)
         self.assertEqual(response.status_code, 200)
 
     def test_sub_index_page_drops_old_blogs(self):
         client = Client()
-        response = client.get('/j34/')
+        category = self.cat1
+        response = client.get(f'/j34/category_blogs/{category.pk}/')
         contain_text = "its existence as a separate"
         not_contain_text = "New Mexico officials"
         self.assertContains(response, contain_text)
