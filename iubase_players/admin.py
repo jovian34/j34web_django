51465d9be5ffc2bbd79e1cd55d0c576b6a828896
Carl James <carl@jovian34.com>
2022-09-04 00:07:59 -0400

    iubase_players admin registered

--- iubase_players/admin.py
+++ iubase_players/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Player, League, Level, Team, Transaction, Position, TransactionType, Transaction, Academic, Article, Headshot, Photo, PlayerSocial, SocialPlatform, TeamSocial
+
+admin.site.register(Player)
+admin.site.register(League)
+admin.site.register(Level)
+admin.site.register(Team)
+admin.site.register(Transaction)
+admin.site.register(Position)
+admin.site.register(TransactionType)
+admin.site.register(Academic)
+admin.site.register(Article)
+admin.site.register(Headshot)
+admin.site.register(Photo)
+admin.site.register(PlayerSocial)
+admin.site.register(SocialPlatform)
+admin.site.register(TeamSocial)
