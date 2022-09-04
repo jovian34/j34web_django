31ace0f671e29c7e70b88174983b437b117041c9
Carl James <carl@jovian34.com>
2022-09-04 08:50:22 -0400

    renamed proj django_project

--- django_project/settings.py
+++ django_project/settings.py
@@ -7,14 +7,8 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get("DEVELOP")))
 
 if not bool(int(os.environ.get("DEVELOP"))):
@@ -56,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "j34web_django.urls"
+ROOT_URLCONF = "django_project.urls"
 
 TEMPLATES = [
     {
@@ -74,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "j34web_django.wsgi.application"
+WSGI_APPLICATION = "django_project.wsgi.application"
 
 DATABASES = {
     "default": {
@@ -90,7 +84,6 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -109,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
 
@@ -123,10 +115,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [str(BASE_DIR.joinpath("j34web_django/static"))]
+STATICFILES_DIRS = [str(BASE_DIR.joinpath("django_project/static"))]
 
 if not bool(int(os.environ.get("DEVELOP"))):
     # added due to security warnings
