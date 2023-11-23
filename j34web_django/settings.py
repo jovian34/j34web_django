323ddaa99454e218e4aeb3444c07b4a3a67b4e47
Carl James <carl@jovian34.com>
2023-11-23 14:00:16 -0500

    upgrade deps ATP

--- j34web_django/settings.py
+++ j34web_django/settings.py
@@ -1,8 +1,9 @@
-from dotenv import load_dotenv
+from environs import Env
 from pathlib import Path
 import os
 
-load_dotenv()
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
