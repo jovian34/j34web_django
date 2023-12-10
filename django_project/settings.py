eb75369c0391cec3b74a4933fa7965e00a1b884d
Carl James <carl@jovian34.com>
2023-12-10 15:52:11 -0500

    putting static back in project

--- django_project/settings.py
+++ django_project/settings.py
@@ -140,4 +140,7 @@ if not bool(int(os.environ.get("DEVELOP"))):
 
     SESSION_COOKIE_SECURE = True
 
-    STATIC_ROOT = os.path.join(BASE_DIR, "/var/www/static/")
+    STATIC_ROOT = "/home/carl/j34web_django/django_project/static/"
+
+project_version = "0.3.0"
+os.environ.setdefault("PROJECT_VERSION", project_version)
