d7190f27e4ce22d8c67fc1b51f57838782c239bb
Carl James <carl@jovian34.com>
2023-12-15 08:39:10 -0500

    tweaked CSS for desktop ATP

--- django_project/settings.py
+++ django_project/settings.py
@@ -142,5 +142,5 @@ if not bool(int(os.environ.get("DEVELOP"))):
 
     STATIC_ROOT = os.path.join(BASE_DIR, 'django_project/static/')
 
-project_version = "0.4.4" # fixed markdown phot width 2023-12-14
+project_version = "0.4.5" # tweaked CSS for desktop 2023-12-15
 os.environ.setdefault("PROJECT_VERSION", project_version)
