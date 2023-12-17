7642042bb6aba5ae885c0df7371170120c0c003e
Carl James <carl@jovian34.com>
2023-12-17 14:21:23 -0500

    style form + fix Dj6 dep warning in forms.py ATP

--- j34main/forms.py
+++ j34main/forms.py
@@ -1,6 +1,17 @@
 from django.forms import ModelForm
+from django.db import models
 from .models import Content
 
+def urlfields_assume_https(db_field, **kwargs):
+    """
+    ModelForm.Meta.formfield_callback function to assume HTTPS for scheme-less
+    domains in URLFields.
+    This may not be needed starting with Django 6.0
+    per https://adamj.eu/tech/2023/12/07/django-fix-urlfield-assume-scheme-warnings/ 
+    """
+    if isinstance(db_field, models.URLField):
+        kwargs["assume_scheme"] = "https"
+    return db_field.formfield(**kwargs)
 
 class ContentForm(ModelForm):
     class Meta:
@@ -14,3 +25,4 @@ class ContentForm(ModelForm):
             "content",
             "categories",
         ]
+        formfield_callback = urlfields_assume_https
