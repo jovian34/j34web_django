b937cee12103f32705bfc1adae1880a8f64fd89e
Carl James <carl@jovian34.com>
2023-12-22 10:18:55 -0500

    added a form not yet in use to sanitize HTML add-on content ATP

--- j34main/forms.py
+++ j34main/forms.py
@@ -1,6 +1,10 @@
+import nh3
+
 from django.forms import ModelForm
+from django import forms
 from django.db import models
 from .models import Content
+
 
 def urlfields_assume_https(db_field, **kwargs):
     """
@@ -12,6 +16,7 @@ def urlfields_assume_https(db_field, **kwargs):
     if isinstance(db_field, models.URLField):
         kwargs["assume_scheme"] = "https"
     return db_field.formfield(**kwargs)
+
 
 class ContentForm(ModelForm):
     class Meta:
@@ -26,3 +31,23 @@ class ContentForm(ModelForm):
             "categories",
         ]
         formfield_callback = urlfields_assume_https
+
+
+class HtmlSanitizedCharField(forms.CharField):
+    def to_python(self, value):
+        value = super().to_python(value)
+        if value not in self.empty_values:
+            value = nh3.clean(value)
+        return value
+    
+
+class AdditionalContentHtmlForm(forms.Form):
+    order = forms.IntegerField(label="Order")
+    is_raw_html = forms.BooleanField(label="Is this raw HTML?")
+    additional_content = HtmlSanitizedCharField(label="HTML Content")
+
+
+class AdditionalContentMarkdownForm(forms.Form):
+    order = forms.IntegerField(label="Order")
+    is_raw_html = forms.BooleanField(label="Is this raw HTML?")
+    additional_content = forms.CharField(label="Markdown Content")
