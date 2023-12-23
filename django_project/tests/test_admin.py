8776d4c9210914d9da414683ad65f676029af35d
Carl James <carl@jovian34.com>
2023-12-23 16:13:31 -0500

    removed helium from testing

--- django_project/tests/test_admin.py
+++ django_project/tests/test_admin.py
@@ -1,8 +1,5 @@
 import pytest
 from django.contrib.auth.models import User
-from django.test import LiveServerTestCase
-from helium import start_chrome, kill_browser
-import os
 
 
 @pytest.fixture()
@@ -19,12 +16,3 @@ def test_set_check_passphrase(user_1):
     user_1.set_password("This is my new passphrase")
     assert user_1.check_password("This is my new passphrase") is True
     assert user_1.check_password("This is NOT my new passphrase") is False
-
-
-class TestChrome(LiveServerTestCase):
-    def test_admin_page_renders_in_browser(self):
-        driver = start_chrome(headless=True)
-        admin_path = f"{self.live_server_url}/{os.getenv('ADMIN_WORD')}/"
-        driver.get(admin_path)
-        assert "Log in | jovian34 website" in driver.title
-        kill_browser()
